d9d1be1941ab2dc32163cfc4f6464b17a301ba01
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-04-21 01:17:05 -0500

    Creates UICollectionView+Published page

---
+++ Combine+UIKit.playground/Pages/UICollectionView+Published.xcplaygroundpage/Sources/MainViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// WIP
+public class MainViewController: UICollectionViewController {
+
+    // MARK: - Initializers
+
+    public init() {
+        super.init(collectionViewLayout: UICollectionViewLayout())
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
+
