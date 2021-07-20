3f89db193ce0642b25718aead02e46454bbe7587
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-20 00:57:36 -0500

    Adds basic ui for 'Responding to user input' page

--- Combine+UIKit.playground/Pages/Responding to user input.xcplaygroundpage/Contents.swift
+++ Combine+UIKit.playground/Pages/Responding to user input.xcplaygroundpage/Contents.swift
@@ -1,7 +1,60 @@
-//: [Previous](@previous)
+import PlaygroundSupport
+import UIKit
 
-import Foundation
+public class MainViewController: UIViewController {
 
-var greeting = "Coming soon"
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
 
-//: [Next](@next)
+        return stackView
+    }()
+
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Test"
+        label.textAlignment = .center
+
+        return label
+    }()
+
+    lazy var slider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.tintColor = .red
+
+        return slider
+    }()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+
+        stackView.addArrangedSubview(slider)
+        stackView.addArrangedSubview(label)
+
+        NSLayoutConstraint.activate([
+            slider.widthAnchor.constraint(equalTo: stackView.widthAnchor)
+        ])
+    }
+
+}
+
+let mainViewController = MainViewController()
+mainViewController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 568)
+PlaygroundPage.current.liveView = mainViewController
+
