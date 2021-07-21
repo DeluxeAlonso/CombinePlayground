2d2b335b0143bdf7d4a9ccaa86cf0f4c162e422a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-21 01:08:23 -0500

    Fixes minor visual issue on UISlider+Input page

--- Combine+SwiftUI.playground/Pages/UISlider+Input.xcplaygroundpage/Contents.swift
+++ Combine+SwiftUI.playground/Pages/UISlider+Input.xcplaygroundpage/Contents.swift
@@ -17,4 +17,5 @@ struct MainView: View {
 }
 
 let mainViewController = UIHostingController(rootView: MainView())
+mainViewController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 568)
 PlaygroundPage.current.liveView = mainViewController
