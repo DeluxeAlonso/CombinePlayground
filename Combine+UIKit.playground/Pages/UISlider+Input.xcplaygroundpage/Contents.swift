78a3bfcdba76d95ac1acc342699cc83e618d9580
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-23 00:04:41 -0500

    Adds minor fixes

--- Combine+UIKit.playground/Pages/UISlider+Input.xcplaygroundpage/Contents.swift
+++ Combine+UIKit.playground/Pages/UISlider+Input.xcplaygroundpage/Contents.swift
@@ -4,4 +4,3 @@ import PlaygroundSupport
 let mainViewController = MainViewController()
 mainViewController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 568)
 PlaygroundPage.current.liveView = mainViewController
-
