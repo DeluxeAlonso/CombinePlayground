a5b21e5aed3316d226cdd8c469034c24dfc7abc0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-31 11:14:16 -0500

    Adds basic Zip implementation to Publishers.Zip page

--- Combine.playground/Pages/Publishers.Zip.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Publishers.Zip.xcplaygroundpage/Contents.swift
@@ -1,7 +1,28 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+var cancellables = Set<AnyCancellable>()
 
-//: [Next](@next)
+let firstNotification = Notification(name: Notification.Name("first"))
+let firstPublisher = NotificationCenter.default.publisher(for: firstNotification.name)
+
+let secondNotification = Notification(name: Notification.Name("second"))
+let secondPublisher = NotificationCenter.default.publisher(for: secondNotification.name)
+
+// Zip waits for the two zipped publishers to emit values, otherwise it won't receive a new value
+let _ = Publishers.Zip(firstPublisher, secondPublisher)
+    .sink { val in
+        print("\(val) zipped")
+    }.store(in: &cancellables)
+
+print("Post first")
+NotificationCenter.default.post(firstNotification)
+
+print("Post second")
+NotificationCenter.default.post(secondNotification)
+
+print("Post third")
+NotificationCenter.default.post(firstNotification)
+
+print("Post fourth")
+NotificationCenter.default.post(secondNotification)
