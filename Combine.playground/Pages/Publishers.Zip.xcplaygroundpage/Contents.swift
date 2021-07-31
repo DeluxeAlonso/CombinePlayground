a7e129d18651c1b33e3d451d9707be3c5334a294
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-31 11:24:48 -0500

    Adds implementation comments on Publishers.Zip page

--- Combine.playground/Pages/Publishers.Zip.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Publishers.Zip.xcplaygroundpage/Contents.swift
@@ -9,11 +9,21 @@ let firstPublisher = NotificationCenter.default.publisher(for: firstNotification
 let secondNotification = Notification(name: Notification.Name("second"))
 let secondPublisher = NotificationCenter.default.publisher(for: secondNotification.name)
 
-// Zip waits for the two zipped publishers to emit values, otherwise it won't receive a new value
+// Two ways to use zip: Publishers.Zip instance and Publisher's zip instance function.
+// Both have the same behavior:
+
 let _ = Publishers.Zip(firstPublisher, secondPublisher)
-    .sink { val in
-        print("\(val) zipped")
+    .sink { value in
+        print("\(value) zipped")
     }.store(in: &cancellables)
+
+let _ = firstPublisher.zip(secondPublisher)
+    .sink { value in
+        print("\(value) zipped - with Publisher's zip intance function")
+    }.store(in: &cancellables)
+
+// Zip waits for the two zipped publishers to emit values,
+// otherwise it won't receive a new value
 
 print("Post first")
 NotificationCenter.default.post(firstNotification)
