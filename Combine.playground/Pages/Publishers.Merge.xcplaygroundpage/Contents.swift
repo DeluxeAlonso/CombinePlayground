ec32b4f0ce661630fab086c810e82975aba3eb97
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-02 23:41:43 -0500

    Adds base scenario for Publishers.Merge page

--- Combine.playground/Pages/Publishers.Merge.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Publishers.Merge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+var cancellables = Set<AnyCancellable>()
 
-//: [Next](@next)
+let firstNotification = Notification(name: Notification.Name("first"))
+let firstPublisher = NotificationCenter.default.publisher(for: firstNotification.name)
+
+let secondNotification = Notification(name: Notification.Name("second"))
+let secondPublisher = NotificationCenter.default.publisher(for: secondNotification.name)
+
+// TODO: - Merge implementation here
+
+// Zip waits for the two zipped publishers to emit values,
+// otherwise it won't receive a new value
+
+print("Post first")
+NotificationCenter.default.post(firstNotification)
+
+print("Post second")
+NotificationCenter.default.post(secondNotification)
+
+print("Post third")
+NotificationCenter.default.post(firstNotification)
+
+print("Post fourth")
+NotificationCenter.default.post(secondNotification)
