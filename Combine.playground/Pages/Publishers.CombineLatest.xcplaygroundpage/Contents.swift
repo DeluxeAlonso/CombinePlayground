76efdcc72aa81ca78e78230b5b2c37f74ec75af4
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-25 01:17:57 -0500

    Adds docs for results in combineLatest page

--- Combine.playground/Pages/Publishers.CombineLatest.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Publishers.CombineLatest.xcplaygroundpage/Contents.swift
@@ -14,12 +14,14 @@ let secondPublisher = NotificationCenter.default.publisher(for: secondNotificati
 
 let _ = Publishers.CombineLatest(firstPublisher, secondPublisher)
     .sink(receiveValue: { value in
-        print("\(value) combined")
+        print("\(value.0) - first publisher - combineLatest")
+        print("\(value.1) - second publisher - combineLatest")
     }).store(in: &cancellables)
 
 let _ = firstPublisher.combineLatest(secondPublisher)
     .sink { value in
-        print("\(value) combined - with Publisher's combineLatest intance function")
+        print("\(value.0) - first publisher - combineLatest - instance method")
+        print("\(value.1) - second publisher - combineLatest - instance method")
     }.store(in: &cancellables)
 
 // Emits the latest value of each publisher when one of them emits a new value.
