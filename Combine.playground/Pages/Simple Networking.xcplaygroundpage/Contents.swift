62534185abde3c4367c488b689e55f1a7a1b0f9d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-27 01:07:02 -0500

    Adds non combine implementation for Simple networking page

--- Combine.playground/Pages/Simple Networking.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Simple Networking.xcplaygroundpage/Contents.swift
@@ -1,1 +1,26 @@
+import Foundation
+import Combine
 
+// MARK: - Non-combine approach
+
+func fetchURL<T: Decodable>(_ url: URL,
+                            completion: @escaping (Result<T, Error>) -> Void) {
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+
+        guard let data = data else {
+            // This should never happen
+            return
+        }
+
+        do {
+            let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+            completion(.success(decodedResponse))
+        } catch {
+            completion(.failure(error))
+        }
+    }.resume()
+}
