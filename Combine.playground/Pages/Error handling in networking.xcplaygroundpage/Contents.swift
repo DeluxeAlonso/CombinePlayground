ed24909a2a260ad75948bc4aae9db51fb3ee3fa1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-11-01 00:50:01 -0500

    Updates error handling in networking page

--- Combine.playground/Pages/Error handling in networking.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Error handling in networking.xcplaygroundpage/Contents.swift
@@ -4,16 +4,33 @@ import Foundation
 struct CustomError: Error, Decodable {}
 
 func fetch<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
-  URLSession.shared.dataTaskPublisher(for: url)
-    .tryMap({ result in
-      let decoder = JSONDecoder()
+    URLSession.shared.dataTaskPublisher(for: url)
+        .tryMap({ result in
+            let decoder = JSONDecoder()
 
-      guard let urlResponse = result.response as? HTTPURLResponse, (200...201).contains(urlResponse.statusCode) else {
-        let error = try decoder.decode(CustomError.self, from: result.data)
-        throw error
-      }
+            // We check if status code is a success one
+            guard let urlResponse = result.response as? HTTPURLResponse,
+                  (200...201) ~= urlResponse.statusCode else {
+                      let error = try decoder.decode(CustomError.self, from: result.data)
+                      throw error
+                  }
 
-      return try decoder.decode(T.self, from: result.data)
-    })
-    .eraseToAnyPublisher()
+            return try decoder.decode(T.self, from: result.data)
+        })
+        .eraseToAnyPublisher()
 }
+
+var cancellables = Set<AnyCancellable>()
+let url = URL(string: "https://www.google.com")
+
+struct TestModel: Decodable {}
+
+fetch(url!)
+    .sink(receiveCompletion: { completion in
+        if case .failure(let error) = completion,
+           let error = error as? CustomError {
+            print(error)
+        }
+    }, receiveValue: { (model: TestModel) in
+        print(model)
+    }).store(in: &cancellables)
