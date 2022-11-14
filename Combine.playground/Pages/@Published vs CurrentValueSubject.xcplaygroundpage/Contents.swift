ebafe503dc4f30693569c3af945bb03a2ad5f7c6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-11-13 19:16:13 -0500

    Implements Published vs CurrentValueSubject page

--- Combine.playground/Pages/@Published vs CurrentValueSubject.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/@Published vs CurrentValueSubject.xcplaygroundpage/Contents.swift
@@ -1,3 +1,21 @@
 import Combine
 
+class ListViewModel {
+
+    var currentValueSubjectTitle = CurrentValueSubject<String, Never>("Initial title")
+
+    /// Published property wrapper can only be used in classes.
+    @Published var publishedTitle = "Initial title"
+    
+}
+
+let viewModel = ListViewModel()
+
+viewModel.currentValueSubjectTitle.sink(receiveValue: { value in
+    print("CurrentValueSubject - Same emitted value and unerlying value? \(viewModel.currentValueSubjectTitle.value == value)")
+})
+
+viewModel.$publishedTitle.sink(receiveValue: { value in
+    print("@Published - Same emitted value and unerlying value? \(viewModel.currentValueSubjectTitle.value == value)")
+})
 
