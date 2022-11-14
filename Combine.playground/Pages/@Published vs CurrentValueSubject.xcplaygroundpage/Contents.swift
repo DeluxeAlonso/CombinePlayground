a1654d6626bc1edfc79123bc52ca987c3dcd3271
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-11-14 00:51:41 -0500

    Adds additional comments to Published vs CurrentValueSubject page

--- Combine.playground/Pages/@Published vs CurrentValueSubject.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/@Published vs CurrentValueSubject.xcplaygroundpage/Contents.swift
@@ -11,13 +11,23 @@ class ListViewModel {
 
 let viewModel = ListViewModel()
 
+// CurrentValueSubject will update its value before emitting the value to its subscribers.
 viewModel.currentValueSubjectTitle.sink(receiveValue: { value in
     print("CurrentValueSubject - Same emitted value and unerlying value? \(viewModel.currentValueSubjectTitle.value == value)")
 })
 
+// @Published value will update its underlying value after emitting the value to its subscribers.
 viewModel.$publishedTitle.sink(receiveValue: { value in
     print("@Published - Same emitted value and unerlying value? \(viewModel.publishedTitle == value)")
 })
 
 viewModel.currentValueSubjectTitle.value = "New title"
 viewModel.publishedTitle = "New title"
+
+/*
+ Output:
+ CurrentValueSubject - Same emitted value and unerlying value? true
+ @Published - Same emitted value and unerlying value? true
+ CurrentValueSubject - Same emitted value and unerlying value? true
+ @Published - Same emitted value and unerlying value? false
+*/
